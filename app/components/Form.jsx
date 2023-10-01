7fb39f23a4132633d4c0dd43764a010813694554
Regina Sherman <92479371+ReginaSherman@users.noreply.github.com>
2023-10-01 15:40:27 -0500

    change input

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -64,7 +64,7 @@ export default function App() {
           name="message"
           required
         />
-        <button type="submit">Submit</button>
+        <input type="submit" value='submit'/>
       </form>
     </div>
   );

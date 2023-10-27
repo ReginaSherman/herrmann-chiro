b7cbf484b098b8140beb913bfcc02a6c82063ce5
Regina Sherman <92479371+ReginaSherman@users.noreply.github.com>
2023-10-27 07:09:38 -0500

    form tweaks;

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,9 +1,10 @@
 "use client";
-import { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 export default function Form() {
   const form = useRef(null);
+  const [isSubmitted, setSubmitted] = useState(false);
 
   const sendEmail = (event) => {
     event.preventDefault();
@@ -23,8 +24,9 @@ export default function Form() {
         )
         .then(
           (result) => {
-            alert(result.text);
-            console.log("success");
+            // Reset the form and set the submitted state to true
+            form.current.reset();
+            setSubmitted(true);
           },
           (error) => {
             alert(error.text);
@@ -36,39 +38,47 @@ export default function Form() {
 
   return (
     <div className="contact-form">
-      <h2>Send us a message for more info!</h2>
-      <p>Get in touch for a healthier you.</p>
-      <form ref={form} className="inner-form" onSubmit={sendEmail}>
-        <input
-          className="fullName"
-          type="text"
-          placeholder="Full name*"
-          name="user_name"
-          required
-        />
-        <input
-          className="email"
-          type="text"
-          placeholder="Email*"
-          name="user_email"
-          required
-        />
-        <input
-          className="subject"
-          type="text"
-          placeholder="Subject*"
-          name="subject"
-          required
-        />
-        <textarea
-          className="message"
-          type="text"
-          placeholder="Message*"
-          name="message"
-          required
-        />
-        <input className="submit" type="submit" value="Send" />
-      </form>
+      {isSubmitted ? (
+        <div className="submission-message">
+          Thank you for your message! We will get back to you as soon as we can.
+        </div>
+      ) : (
+        <div>
+          <h2>Send us a message for more info!</h2>
+          <p>Get in touch for a healthier you.</p>
+          <form ref={form} className="inner-form" onSubmit={sendEmail}>
+            <input
+              className="fullName"
+              type="text"
+              placeholder="Full name*"
+              name="user_name"
+              required
+            />
+            <input
+              className="email"
+              type="text"
+              placeholder="Email*"
+              name="user_email"
+              required
+            />
+            <input
+              className="subject"
+              type="text"
+              placeholder="Subject*"
+              name="subject"
+              required
+            />
+            <textarea
+              className="message"
+              type="text"
+              placeholder="Message*"
+              name="message"
+              required
+            />
+            <input className="submit" type="submit" value="Send" />
+          </form>
+        </div>
+      )}
     </div>
   );
 }
--- app/components/HeroSlider.jsx
+++ app/components/HeroSlider.jsx
@@ -38,12 +38,9 @@ export default function HeroSlider() {
     // listen for Swiper events using addEventListener
     swiperElRef.current.addEventListener("progress", (e) => {
       const [swiper, progress] = e.detail;
-      console.log(progress);
     });
 
-    swiperElRef.current.addEventListener("slidechange", (e) => {
-      console.log("slide changed");
-    });
+    swiperElRef.current.addEventListener("slidechange", (e) => {});
   }, []);
   return (
     <>
